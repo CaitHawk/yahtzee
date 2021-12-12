9d9b4c8084188242db726b4ed673edc9f1b05461
Caitlin Neathawk <neathawkcaitlin@gmail.com>
2021-12-11 18:14:53 -0800

    final

--- src/Die.js
+++ src/Die.js
@@ -4,6 +4,7 @@ import "./Die.css";
 class Die extends Component {
   static defaultProps = {
     numberWords : ['one', 'two', 'three', 'four', 'five', 'six'],
+    val: 5
   }
   constructor(props){
     super(props)
@@ -15,12 +16,15 @@ class Die extends Component {
   
 
   render() {
-    let classes = `Die fas fa-dice-${this.props.numberWords[this.props.val - 1]} fa-5x `;
-    if(this.props.locked) { classes += 'Die-locked'}
+    const {numberWords, val, locked, disabled, rolling} = this.props;
+    let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-5x `;
+    if(locked) { classes += 'Die-locked' }
+    if(rolling){ classes += 'Die-rolling' }
     return (
       <i
         className={classes}
         onClick={this.handleClick}
+        disabled={disabled}
       />
     );
   }
